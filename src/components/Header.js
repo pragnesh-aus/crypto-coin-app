9e21cb53dff490e89c3076984abd0151066b2af1
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-10-26 11:38:14 +1100

    Update Header.js
    add dark theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,72 @@
+import {
+  AppBar,
+  Container,
+  createTheme,
+  makeStyles,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  ThemeProvider,
+} from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: 'gold',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
+}));
 
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+  // history hooks from react router dom
+  const history = useHistory();
+  // dark theme from Material UI
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#fff',
+      },
+      type: 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push('/')}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto Coin App
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+
+              //  value={currency}
+              // onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={'USD'}>USD</MenuItem>
+              <MenuItem value={'AUD'}>AUD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Header;
