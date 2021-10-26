a6c2162ca5ce3dd04feb1feaf9b5aa8fecd941ce
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-10-26 18:05:00 +1100

    Install React alice carousel

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,5 +1,8 @@
 import { makeStyles } from '@material-ui/core';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { TrendingCoins } from '../../config/api';
+import { CryptoState } from '../../CryptoContext';
 
 const useStyles = makeStyles((theme) => ({
   carousel: {
@@ -9,7 +12,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 const Carousel = () => {
+  const [trending, setTrending] = useState([]);
   const classes = useStyles();
+  const { currency } = CryptoState();
+  const fetchTrendingCoins = async () => {
+    // fetch data via api call from api.js file in config folder in app
+    const { data } = await axios.get(TrendingCoins(currency));
+    // setting the fetched data
+    setTrending(data);
+  };
+
+  console.log(trending);
+  // called function when app load first time
+  useEffect(() => {
+    fetchTrendingCoins();
+  }, [currency]); // pass currency every time when user changed the currency
   return <div className={classes.carousel}>Carousel</div>;
 };
 
